a5c2fb1b22d825abe29d01f3e91e2bfab7b66fe2
thibault <thibault.latrace@gmail.com>
2021-11-12 00:04:07 +0100

    downscaling refacto

--- constants.py
+++ constants.py
@@ -12,15 +12,16 @@ local_machine = True
 if local_machine:
     DATA_DIR_ROOT = Path(r"C:\Users\thiba\OneDrive - CentraleSupelec\Mission_JCS_IA_peinture\files")
     MASKS_DIR_PATH = DATA_DIR_ROOT / "labels_masks/all"
-else:
+    CHECKPOINT_DIR_PATH = DATA_DIR_ROOT / "checkpoints/2021_10_10__18_16_44"
+else:  # aws instance
     DATA_DIR_ROOT = Path(r"/home/ec2-user/data")
     MASKS_DIR_PATH = DATA_DIR_ROOT / "labels_masks"
+    CHECKPOINT_DIR_PATH = DATA_DIR_ROOT / "checkpoints/2021_10_03__18_31_03"
 
 IMAGES_DIR_PATH = DATA_DIR_ROOT / "images"
 PATCHES_DIR_PATH = DATA_DIR_ROOT / "patches/256x256"
 PREDICTIONS_DIR_PATH = DATA_DIR_ROOT / "predictions"
 CHECKPOINT_ROOT_DIR_PATH = DATA_DIR_ROOT / "checkpoints"
-CHECKPOINT_DIR_PATH = DATA_DIR_ROOT / "checkpoints/2021_10_03__18_31_03"
 # OUTPUT_PATH = DATA_DIR_ROOT / "test.png"
 # IMAGE_PATCH_PATH = DATA_DIR_ROOT / "patches/1/1/image/patch_1.jpg"
 IMAGE_PATH = DATA_DIR_ROOT / "images/_DSC0246/_DSC0246.jpg"
@@ -85,10 +86,10 @@ INPUT_SHAPE = 256
 BATCH_SIZE = 32  # 32 is a frequently used value
 N_CLASSES = 9
 N_EPOCHS = 10
-N_PATCHES_LIMIT = 10000
+N_PATCHES_LIMIT = 100
 TEST_PROPORTION = 0.2
 PATCH_OVERLAP = 40  # 20 not enough, 40 great
-PATCH_COVERAGE_PERCENT_LIMIT = 90
+PATCH_COVERAGE_PERCENT_LIMIT = 75
 ENCODER_KERNEL_SIZE = 3
 LINEARIZER_KERNEL_SIZE = 3
 N_CPUS = 4
@@ -104,4 +105,4 @@ LOSS_FUNCTION = keras.losses.categorical_crossentropy
 # METRICS = [keras.metrics.MeanIoU(N_CLASSES)]
 METRICS = [keras.metrics.categorical_accuracy, keras.metrics.MeanIoU(N_CLASSES)]
 DOWNSCALE_FACTORS = (6, 6, 1)
-DATA_AUGMENTATION = True
+DATA_AUGMENTATION = False
--- deep_learning/preprocessing/downscaling.py
+++ deep_learning/preprocessing/downscaling.py
@@ -1,6 +1,7 @@
-from pathlib import Path
+import tensorflow as tf
 
-import numpy as np
+from pathlib import Path
+from math import ceil
 from skimage.transform import downscale_local_mean
 
 from constants import DOWNSCALE_FACTORS, IMAGE_PATH
@@ -8,14 +9,35 @@ from dataset_utils.image_utils import decode_image
 from dataset_utils.plotting_tools import plot_image_from_array
 
 
-def downscale_image(image_path: Path, downscale_factors: tuple) -> np.ndarray:
-    image_array = decode_image(image_path)
-    downscaled_array = downscale_local_mean(image_array, downscale_factors).astype(int)
-    return downscaled_array
+def downscale_image(image_path: Path, downscale_factors: tuple) -> tf.Tensor:
+    """
+    Downscale the image.
+
+    :param image_path: Path of the image to downscale.
+    :param downscale_factors: Factors on which we want to downscale the image.
+    :return: A tensor of shape ceil(image_tensor.shape / downscale_factors).
+    """
+    image_tensor = decode_image(image_path)
+    downscaled_array = downscale_local_mean(image=image_tensor, factors=downscale_factors).astype(int)
+    downscaled_tensor = tf.constant(downscaled_array, dtype=tf.int32)
+
+    # checks if the dimensions of the output are correct
+    assert downscaled_tensor.shape[0] == ceil(
+        image_tensor.shape[0] / downscale_factors[0]
+    ), f"Downscaled tensor has height shape {downscaled_tensor.shape[0]} while it should have {ceil(image_tensor.shape[0] / downscale_factors[0])}"
+    assert downscaled_tensor.shape[1] == ceil(
+        image_tensor.shape[1] / downscale_factors[1]
+    ), f"Downscaled tensor has width shape {downscaled_tensor.shape[1]} while it should have {ceil(image_tensor.shape[1] / downscale_factors[1])}"
+    assert downscaled_tensor.shape[2] == ceil(
+        image_tensor.shape[2] / downscale_factors[2]
+    ), f"Downscaled tensor has channels shape {downscaled_tensor.shape[2]} while it should have {ceil(image_tensor.shape[2] / downscale_factors[2])}"
+
+    return downscaled_tensor
 
 
 def plot_downscale_image(image_path: Path, downscale_factors: tuple) -> None:
-    downscaled_array = downscale_image(image_path, downscale_factors)
+    downscaled_tensor = downscale_image(image_path, downscale_factors)
+    downscaled_array = downscaled_tensor.numpy()
     plot_image_from_array(downscaled_array)
 
 
