6676fd66110fbcb6df210249623cb3750b8532b3
thibault <thibault.latrace@gmail.com>
2022-02-27 15:30:49 +0100

    delete scratches folder

--- scratches/shell_scripts/mask_downloader.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python labelbox_utils/mask_downloader.py --json-path "C:\Users\thiba\OneDrive - CentraleSupelec\Mission_JCS_IA_peinture\labelbox_export_json\final_json\export-2021-07-30T15_56_44.814Z.json" --output-dir-path "C:\Users\thiba\PycharmProjects\mission_IA_JCS\files\labels_masks\marie"
